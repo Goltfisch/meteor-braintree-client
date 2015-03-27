c63650da0ae0ff76f8e2a65c953fe3adec8fcc47
Philipp Muens <raute1337@gmx.de>
2015-03-27 15:55:49 +0100

    Add braintree.js-File
    Add braintree.js-File so that the braintree.js library is included in the head-Tag.

---
+++ braintree.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.setAttribute('src', 'https://js.braintreegateway.com/v2/braintree.js');
+document.getElementsByTagName('head')[0].appendChild(script);
